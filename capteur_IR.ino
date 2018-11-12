e4fc88c35e7d5511269f4beed49858c680b25831
Achille <achille.brossier@viacesi.fr>
2018-11-12 18:16:29 +0100

    1.0

---
+++ capteur_IR.ino
@@ -0,0 +1,29 @@
+
+const int led = 9;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led, OUTPUT); 
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ int sensorValue = analogRead(A0);
+  Serial.print(sensorValue);
+  Serial.print("\n");
+  if (sensorValue > 0)
+  { 
+    digitalWrite(led, HIGH);
+  
+    
+    }
+    else 
+    {
+      digitalWrite(led, LOW);
+      }
+
+
+}
